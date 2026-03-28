f5a71cb6d104d0959284171ae82ca94940f4b858
Yahia Abdelsalam <yahiatarek293@gmail.com>
2026-03-28 16:12:51 -0400

    gjg

--- src/Pages/Portofolio.jsx
+++ src/Pages/Portofolio.jsx
@@ -57,6 +57,9 @@ function a11yProps(index) {
 }
 
 const techStacks = [
+  { icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg", language: "C++" },
+  { icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg", language: "Java" },
+  { icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg", language: "Python" },
   { icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", language: "JavaScript" },
   { icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", language: "HTML" },
   { icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", language: "CSS" },
